f0238e574ebe395debb7e3017339022dfa49d884
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-02-22 12:53:31 +0100

    chore: offload min max checking to a dedicated function

--- number.go
+++ number.go
@@ -7,9 +7,7 @@ func (nr *Randomizer) Int() int {
 
 // IntInRange generates a random integer between min (inclusive) and max (exclusive).
 func (nr *Randomizer) IntInRange(min, max int) int {
-	if min >= max {
-		panic("min must be less than max in IntInRange")
-	}
+	checkRange(min, max)
 	return nr.Rand.Intn(max-min) + min
 }
 
@@ -20,9 +18,7 @@ func (nr *Randomizer) UInt() uint {
 
 // UIntInRange generates a random unsigned integer between min (inclusive) and max (exclusive).
 func (nr *Randomizer) UIntInRange(min, max int) uint {
-	if min >= max {
-		panic("min must be less than max in UIntInRange")
-	}
+	checkRange(min, max)
 	return uint(nr.Rand.Uint64())%uint(max-min) + uint(min)
 }
 
@@ -33,9 +29,7 @@ func (nr *Randomizer) UInt32() uint32 {
 
 // UInt32InRange generates a random unsigned integer 32 between min (inclusive) and max (exclusive).
 func (nr *Randomizer) UInt32InRange(min, max int) uint32 {
-	if min >= max {
-		panic("min must be less than max in UInt32InRange")
-	}
+	checkRange(min, max)
 	return nr.Rand.Uint32()%uint32(max-min) + uint32(min)
 }
 
@@ -46,9 +40,7 @@ func (nr *Randomizer) UInt64() uint64 {
 
 // UInt64InRange generates a random unsigned integer 64 between min (inclusive) and max (exclusive).
 func (nr *Randomizer) UInt64InRange(min, max int) uint64 {
-	if min >= max {
-		panic("min must be less than max in UInt64InRange")
-	}
+	checkRange(min, max)
 	return nr.Rand.Uint64()%uint64(max-min) + uint64(min)
 }
 
@@ -59,21 +51,24 @@ func (nr *Randomizer) Int32() int32 {
 
 // Int32InRange generates a random integer 32 between min (inclusive) and max (exclusive).
 func (nr *Randomizer) Int32InRange(min, max int) int32 {
-	if min >= max {
-		panic("min must be less than max in Int32InRange")
-	}
+	checkRange(min, max)
 	return nr.Rand.Int31()%int32(max-min) + int32(min)
 }
 
-// Int32 generates a random integer 64 using the full range of the randomizer.
+// Int64 generates a random integer 64 using the full range of the randomizer.
 func (nr *Randomizer) Int64() int64 {
 	return nr.Rand.Int63()
 }
 
 // Int64InRange generates a random integer 64 between min (inclusive) and max (exclusive).
 func (nr *Randomizer) Int64InRange(min, max int) int64 {
-	if min >= max {
-		panic("min must be less than max in Int64InRange")
-	}
+	checkRange(min, max)
 	return nr.Rand.Int63()%int64(max-min) + int64(min)
+}
+
+// checkRange ensures that min is less than max; otherwise, it panics.
+func checkRange(min, max int) {
+	if min >= max {
+		panic("min must be less than max")
+	}
 }
--- number_test.go
+++ number_test.go
@@ -8,67 +8,67 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenerateRandomNumber(t *testing.T) {
+func TestInt(t *testing.T) {
 	// Setup
 	randomizer := number.NewRandomizer()
-	randomNumber := randomizer.GenerateRandomNumber()
+	randomNumber := randomizer.Int()
 
 	// Assert
-	assert.NotEqual(t, 0, randomNumber, "Generated number should not be 0")
+	assert.NotEqual(t, 0, randomNumber, "randomized number should not be 0")
 }
 
-func TestGenerateRandomNumberInRange(t *testing.T) {
+func TestIntInRange(t *testing.T) {
 	// Setup
 	randomizer := number.NewRandomizer()
 	min := 10
 	max := 20
-	randomNumber := randomizer.GenerateRandomNumberInRange(min, max)
+	randomNumber := randomizer.IntInRange(min, max)
 
 	// Assert
-	assert.GreaterOrEqual(t, randomNumber, min, "Generated number should be greater than or equal to min")
-	assert.Less(t, randomNumber, max, "Generated number should be less than max")
+	assert.GreaterOrEqual(t, randomNumber, min, "randomized number should be greater than or equal to min")
+	assert.Less(t, randomNumber, max, "randomized number should be less than max")
 }
 
-func TestGenerateRandomNumberTypeCheck(t *testing.T) {
+func TestIntTypeCheck(t *testing.T) {
 	// Setup
 	randomizer := number.NewRandomizer()
-	randomNumber := randomizer.GenerateRandomNumber()
+	randomNumber := randomizer.Int()
 	randomNumberType := reflect.TypeOf(randomNumber)
 
 	// Assert
-	assert.Equal(t, "int", randomNumberType.Name(), "Generated number should be of type int")
+	assert.Equal(t, "int", randomNumberType.Name(), "randomized number should be of type int")
 }
 
-func TestGenerateRandomNumberWithSeed(t *testing.T) {
+func TestIntWithSeed(t *testing.T) {
 	// Setup
 	seed := int64(31 * 41 * 53)
 	randomizer := number.NewRandomizerWithSeed(seed)
-	randomNumber := randomizer.GenerateRandomNumber()
+	randomNumber := randomizer.Int()
 
 	// Assert
-	assert.NotEqual(t, 0, randomNumber, "Generated number should not be 0")
+	assert.NotEqual(t, 0, randomNumber, "randomized number should not be 0")
 }
 
-func TestGenerateRandomNumberInRangeWithSeed(t *testing.T) {
+func TestIntInRangeWithSeed(t *testing.T) {
 	// Setup
 	seed := int64(31 * 41 * 53)
 	randomizer := number.NewRandomizerWithSeed(seed)
 	min := 153243212
 	max := 153243213
-	randomNumber := randomizer.GenerateRandomNumberInRange(min, max)
+	randomNumber := randomizer.IntInRange(min, max)
 
 	// Assert
-	assert.GreaterOrEqual(t, randomNumber, min, "Generated number should be greater than or equal to min")
-	assert.Less(t, randomNumber, max, "Generated number should be less than max")
+	assert.GreaterOrEqual(t, randomNumber, min, "randomized number should be greater than or equal to min")
+	assert.Less(t, randomNumber, max, "randomized number should be less than max")
 }
 
-func TestGenerateRandomNumberTypeCheckWithSeed(t *testing.T) {
+func TestIntTypeCheckWithSeed(t *testing.T) {
 	// Setup
 	seed := int64(31 * 41 * 53)
 	randomizer := number.NewRandomizerWithSeed(seed)
-	randomNumber := randomizer.GenerateRandomNumber()
+	randomNumber := randomizer.Int()
 	randomNumberType := reflect.TypeOf(randomNumber)
 
 	// Assert
-	assert.Equal(t, "int", randomNumberType.Name(), "Generated number should be of type int")
+	assert.Equal(t, "int", randomNumberType.Name(), "randomized number should be of type int")
 }
